b6f97c6e28f02a32a2b348986d53c414dbc20a26
pcor4650 <pcor4650@gmail.com>
2022-12-26 16:12:57 +0900

    Parameters

--- real-movie-app/src/routes/Detail.js
+++ real-movie-app/src/routes/Detail.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
+  const { id } = useParams();
+  const getMovie = async () => {
+    const response = await fetch(
+      "https://yts.mx/api/v2/movie_details.json?movie_id=" + id
+    );
+    const data = await response.json();
+    console.log(data);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
   return <h1>Detail</h1>;
 }
 export default Detail;
