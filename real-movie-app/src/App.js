bdeba25eb1421f21417e15b3a4fdad8fa1dcbed1
pcor4650 <pcor4650@gmail.com>
2022-12-26 11:14:54 +0900

    Movie App Part 1

--- real-movie-app/src/App.js
+++ real-movie-app/src/App.js
@@ -1,5 +1,46 @@
+// https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year 사용필요, 회사에서 접근 제한됨
+import { useEffect, useState } from "react";
+
 function App() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+
+  // async - await 사용하여 fetch 이후 수정
+  useEffect(() => {
+    fetch(
+      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.9&sort_by=year"
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>
+                {movie.title} ({movie.year})
+              </h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((genre) => (
+                  <li key={genre}>{genre}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
