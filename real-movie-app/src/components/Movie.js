3a603659d87ff69af420657393ed66e6f9118b9b
pcor4650 <pcor4650@gmail.com>
2023-01-05 18:11:31 +0900

    Movie UI 업데이트

--- real-movie-app/src/components/Movie.js
+++ real-movie-app/src/components/Movie.js
@@ -1,22 +1,74 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
+const MovieContainer = styled.div`
+  background-color: white;
+  margin-bottom: 70px;
+  font-weight: 300;
+  padding: 20px;
+  border-radius: 15px;
+  color: #adaeb9;
+  display: grid;
+  grid-template-columns: minmax(150px, 1fr) 2fr;
+  grid-gap: 20px;
+  text-decoration: none;
+  color: inherit;
+  box-shadow: 0 13px 27px -5px rgba(50, 50, 93, 0.25),
+    0 8px 16px -8px rgba(0, 0, 0, 0.3), 0 -6px 16px -6px rgba(0, 0, 0, 0.025);
+`;
+
+const StyledImg = styled.img`
+  position: relative;
+  top: -50px;
+  max-width: 150px;
+  border-radius: 15px;
+  width: 100%;
+  margin-right: 30px;
+  box-shadow: 0 30px 60px -12px rgba(50, 50, 93, 0.25),
+    0 18px 36px -18px rgba(0, 0, 0, 0.3), 0 -12px 36px -8px rgba(0, 0, 0, 0.025);
+`;
+
+const StyledTitle = styled.h2`
+  margin: 0;
+  font-weight: 300;
+  text-decoration: none;
+  margin-bottom: 5px;
+  font-size: 24px;
+  color: #2c2c2c;
+`;
+
+const StyledGenres = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-wrap: wrap;
+  margin: 5px 0px;
+`;
+
+const StyledList = styled.li`
+  margin-right: 10px;
+  font-size: 14px;
+`;
 function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
-    <div>
-      <img src={coverImg} alt={title} />
-      <h2>
-        <Link to={`/movie/${id}`}>
-          {title} ({year})
-        </Link>
-      </h2>
-      <p>{summary.length ? `${summary.slice(0, 235)}...` : summary}</p>
-      <ul>
-        {genres.map((genre) => (
-          <li key={genre}>{genre}</li>
-        ))}
-      </ul>
-    </div>
+    <MovieContainer>
+      <StyledImg src={coverImg} alt={title} />
+      <div>
+        <StyledTitle>
+          <Link to={`/movie/${id}`}>
+            {title} ({year})
+          </Link>
+        </StyledTitle>
+        <p>{summary.length ? `${summary.slice(0, 235)}...` : summary}</p>
+        <StyledGenres>
+          {genres.map((genre) => (
+            <StyledList key={genre}>{genre}</StyledList>
+          ))}
+        </StyledGenres>
+      </div>
+    </MovieContainer>
   );
 }
 
--- real-movie-app/src/routes/Home.js
+++ real-movie-app/src/routes/Home.js
@@ -1,29 +1,64 @@
 // https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year 사용필요, 회사에서 접근 제한됨
 import { useEffect, useState } from "react";
+import styled from "styled-components";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
+
+const LoaderContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-weight: 600;
+`;
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
 
+  async function getMovies() {
+    try {
+      const response = await fetch(
+        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.9&sort_by=year"
+      );
+      const json = await response.json();
+      setMovies(json.data.movies);
+    } catch (error) {
+      console.error(error.message);
+    }
+    setLoading(false);
+  }
+
   // async - await 사용하여 fetch 이후 수정
   useEffect(() => {
-    fetch(
-      "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.9&sort_by=year"
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
 
   return (
-    <div>
+    <div
+      style={{
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
       {loading ? (
-        <h1>Loading...</h1>
+        <LoaderContainer>
+          <span>...Loading</span>
+        </LoaderContainer>
       ) : (
-        <div>
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(2, minmax(300px, 1fr)",
+            gridGap: "50px",
+            padding: "50px",
+            width: "80%",
+            paddingtop: "70px",
+          }}
+          className={styles.movies}
+        >
           {movies.map((movie) => (
             <Movie
               key={movie.id}
